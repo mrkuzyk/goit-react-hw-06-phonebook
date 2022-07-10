f850d218a7ce8e6ee1dd921b38fb3e6704388308
Marian Kuzyk <mariankuzyk@gmail.com>
2022-07-11 02:49:43 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,23 @@
-import { useState, useEffect} from "react";
+import { useEffect} from "react";
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import style from './App.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import { add, deleted } from "redux/contactsSlice";
+import { filtered } from "redux/searchFilterSlice";
 
 
 export function App() {
-
-  // лінива ініціалізація, щоб кожен раз не рендирився стейт через локал сторедж
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('contacts')) ??
-    [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
-  });
-  const [filter, setFilter] = useState('');
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
 
   const addContact = ({ name, number }) => {
-    // якщо в контактах є вже таке ім'я то видає помилку 
-    if (contacts.some(contact => contact.name === name)) {
-      return alert(`${name} is already in contacts`);
+    
+    if (contacts.some(contact => contact.name === name)) { 
+      return alert(`${name} is already in contacts`); // якщо в контактах є вже таке ім'я то видає помилку 
     };
 
     // якщо ім'я унікальне, то створюю новий контакт з id
@@ -33,41 +27,24 @@ export function App() {
       number,
     };
 
-    // новий контакт додаю спереді, пер тими що були
-    setContacts(prevContacts => [contact, ...contacts])
+    dispatch(add(contact)); // redux - новий контакт додаю спереді, пер тими що були
   };
 
   const deleteContact = (id) => {
-    
-    // шукаю конткакт який має id таке ж, як і було на кнопці видалити і повертаю всі, крім нього
-    setContacts(prevContacts => contacts.filter(contact => contact.id !== id))
+    dispatch(deleted(id)) // redux - шукаю конткакт який має id таке ж, як і було на кнопці видалити і повертаю всі, крім нього
   };
 
   const searchFilter = e => {
-    // отримую значення з інпуту і записую його в стейт
-    setFilter(e.currentTarget.value)
+    dispatch(filtered(e.currentTarget.value)) // отримую значення з інпуту і записую його
   };
 
   const getVisibleContact = () => {
-    // що не було похибки, то підношу всі до великих букв
-    const normalizedFilter = filter.toLowerCase();
-
-    // фільтрую і повертаю тільки ті контакти, які відповідають пошуку
-    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+    const normalizedFilter = filter.toLowerCase(); // що не було похибки, то підношу всі до великих букв
+    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter)); // фільтрую і повертаю тільки ті контакти, які відповідають пошуку
   };
 
-  // useEffect(() => {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
-
-  //   if (parsedContacts) {
-  //     setContacts(parsedContacts)
-  //   };
-  // }, []);
-
   useEffect(() => {
-    // записую в локал сторедж контакти
-    window.localStorage.setItem('contacts', JSON.stringify(contacts))
+    window.localStorage.setItem('contacts', JSON.stringify(contacts)) // записую в локал сторедж контакти
   }, [contacts])
 
   return (
